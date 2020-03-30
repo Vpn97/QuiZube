945c00a515588cf3585cac58a68b0e1429168b96
Vpn97 <vpn0097@gmail.com>
2020-03-30 11:43:51 +0530

    Initial commit

---
+++ app/src/main/java/com/apkzube/quizube/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.apkzube.quizube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+
+}
